6b7a08778e03e6247c481ce8a4e66363edbbf9d5
Anand chaurasia <151776571+AnandCh20@users.noreply.github.com>
2024-06-25 00:02:35 +0530

    MyContact.js

--- src/components/MyContact.js
+++ src/components/MyContact.js
@@ -7,15 +7,15 @@ const MyContact = () => (
         <form>
             <div className="mb-3 bg-dark">
                 <label htmlFor="name" className="form-label bg-dark">Name:</label>
-                <input type="text" className="form-control bg-dark" id="name" name="name" required />
+                <input type="text" className="form-control bg-dark text-white" id="name" name="name" required />
             </div>
             <div className="mb-3 bg-dark">
                 <label htmlFor="email" className="form-label bg-dark">Email:</label>
-                <input type="email" className="form-control bg-dark" id="email" name="email" required />
+                <input type="email" className="form-control bg-dark text-white" id="email" name="email" required />
             </div>
             <div className="mb-3 bg-dark">
                 <label htmlFor="message" className="form-label bg-dark">Message:</label>
-                <textarea className="form-control bg-dark" id="message" name="message" rows="4" required></textarea>
+                <textarea className="form-control bg-dark text-white" id="message" name="message" rows="4" required></textarea>
             </div>
             <button type="submit" className="btn btn-primary">Send Message</button>
         </form>
